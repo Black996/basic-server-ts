97ae1e8a2f4f70155e9ed259d51f23c17d631d50
black996 <rharasis@yahoo.com>
2021-11-04 20:37:54 +0200

    basic node server with typescript

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import cookieSession from "cookie-session";
+import express, { Response, Request } from "express";
+import { router } from "./routes/loginRoutes";
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ["crypto"] }));
+app.use(router);
+
+app.get("/", (req: Request, res: Response) => {
+  if (req.session) {
+    req.session.loggedIn
+      ? res.send(`
+      
+        <div>
+           <p>You are logged in</p>
+           <a href="/logout">Logout</a>
+        </div>
+    `)
+      : res.send(`
+      <div>
+        <p>You are not logged in</p>
+        <a href="/login">Login</a>
+      </div>
+    `);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is on");
+});
---
+++ src/routes/loginRoutes.ts
@@ -0,0 +1,51 @@
+import { Router, Request, Response, NextFunction } from "express";
+
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction) {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  }
+
+  res.status(403).send("Not permitted");
+}
+
+export const router = Router();
+
+router.get("/login", (req: RequestWithBody, res: Response) => {
+  res.send(`
+       <form method="POST" target="/login">
+        <div>
+            <label>Email</label>
+            <input name="email"/>
+        </div>
+        <div>
+        <label>Password</label>
+        <input type="password" name="password"/>
+        </div>
+        <button>Submit</button>
+       </form>
+    `);
+});
+
+router.post("/login", (req: RequestWithBody, res: Response) => {
+  const { email, password } = req.body;
+  if (email && password && email == "hi" && password == "pass") {
+    req.session = { loggedIn: true };
+    res.redirect("/");
+  } else {
+    res.send("You must provide a valid email and password");
+  }
+});
+
+router.get("/logout", (req: Request, res: Response) => {
+  req.session = undefined;
+  res.redirect("/");
+});
+
+router.get("/protected", requireAuth, (req: Request, res: Response) => {
+  res.send("Welcome to the protected route!");
+});
